00fa8d27bb893153157c16c2476de2d113007d67
TrianguloY <correo--correo@hotmail.com>
2015-04-17 20:18:17 +0200

    code cleanup
    removed unused resources from the new activity

--- app/src/main/java/com/trianguloy/llscript/repository/IntentHandle.java
+++ app/src/main/java/com/trianguloy/llscript/repository/IntentHandle.java
@@ -15,7 +15,6 @@ public class IntentHandle extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intent_handle);
         Intent intent=getIntent();
         Log.d("IntentHandler","New intent"+intent.toString());
 
@@ -50,27 +49,5 @@ public class IntentHandle extends Activity {
         intent.putExtra(Constants.extraOpenUrlTime,System.currentTimeMillis());
         startActivity(intent);
         finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_intent_handle, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
